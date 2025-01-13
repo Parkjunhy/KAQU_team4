01cec5ec37970c720c704c4329c17d88e568febb
JH <mat0528@korea.ac.kr>
2025-01-13 22:59:29 +0900

    1.13

--- kaqu_controller/kaqu_controller/Kaquctrl/RobotController.py
+++ kaqu_controller/kaqu_controller/Kaquctrl/RobotController.py
@@ -1,14 +1,59 @@
-import numpy as np
-from KaquCmdManager.CmdManagerNode import Command
-# import Trot, Rest, PID etc...
 
-# class RobotGait(object):
-#     def __init__(self):
-#         self.new_state = Command.get_state()
+import rclpy
+from rclpy.node import Node
+from Kaquctrl import RobotState, BehaviorState  # StateSubscriber가 정의한 상태 클래스
+from TrotGaitController import TrotGaitController
+from RestController import RestController
 
-#     # def gait_sellect(self):
-#     #     if self.new_state == "start":
-#     #         ##
-#     #     elif :
-#     #     self.new_state == "trot":
+
+class RobotController(Node):
+    def __init__(self, state):
+        super().__init__('robot_controller')
+        self.state = state
+
+        # Gait Controllers 초기화
+        self.trot_controller = TrotGaitController()
+        self.rest_controller = RestController()
+        self.rest_controller = StartController()
+
+        self.current_controller = self.rest_controller
+
+    def select_gait(self):
+        """현재 상태에 따라 Gait Controller 선택"""
+        if self.state.trot_event:
+            self.current_controller = self.trot_controller
+            self.state.trot_event = False  
+
+        elif self.state.start_event:
+            self.current_controller = self.start_controller
+            self.state.start_event = False  
             
+        elif self.state.rest_event:
+            self.current_controller = self.rest_controller
+            self.state.rest_event = False  
+
+    def run(self):
+        self.select_gait()
+        self.current_controller.run()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    # StateSubscriber에서 상태 가져오기
+    state = RobotState()
+
+    # RobotController 초기화 및 실행
+    robot_controller = RobotController(state)
+    try:
+        while rclpy.ok():
+            robot_controller.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        robot_controller.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
